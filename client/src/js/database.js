a4572c1e11049214d9ff0388925d20763a12d08b
Meow <tabbathapischke@gmail.com>
2024-02-20 16:27:22 -1000

    change id value to 1 of putDB method; change console log to result.value in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,9 +18,9 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, value: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log('Data saved to the database', result);
+  console.log('Data saved to the database', result.value);
 }
 
 // method that gets all the content from the database
